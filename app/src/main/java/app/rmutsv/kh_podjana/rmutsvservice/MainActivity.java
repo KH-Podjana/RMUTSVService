5815420087314c0406f2db54b7f55366c7ac0168
Podjana <kaew3130@gmail.com>
2017-11-07 10:35:37 +0700

    Main Fragment Success

--- app/src/main/java/app/rmutsv/kh_podjana/rmutsvservice/MainActivity.java
+++ app/src/main/java/app/rmutsv/kh_podjana/rmutsvservice/MainActivity.java
@@ -3,11 +3,23 @@ package app.rmutsv.kh_podjana.rmutsvservice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import app.rmutsv.kh_podjana.rmutsvservice.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+  //      Add Fragment to Activity
+        if (savedInstanceState == null) {
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentFragmentMain, new MainFragment()).commit(); //การ add fragment
+
+        }
+
+    } // Main Method
+
+}//Main Class
